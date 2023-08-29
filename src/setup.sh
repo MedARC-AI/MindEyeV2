c5bce1b8a80c806464455b3fb198280f7b595cfe
Paul Scotti <paulscotti@ip-172-64-56-121.us-west-2.compute.internal>
2023-08-29 18:29:41 +0000

    add setup.sh; note this is untested

---
+++ src/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Commands to setup a new conda environment and install all the necessary packages
+# See the environment.yaml file for "conda env export > environment.yaml" after running this.
+
+set -e
+
+conda create -n fmri python=3.10.8 -y
+conda activate fmri
+
+conda install numpy matplotlib tqdm scikit-image jupyterlab -y
+
+pip install accelerate webdataset clip pandas matplotlib ftfy regex kornia umap-learn h5py
+pip install torchvision==0.15.2 torch==2.0.1
+pip install diffusers
