688aa697acb90b8ed7fd7dfdb5c8c9904eead7b3
PaulScotti <paul.scotti@stability.ai>
2024-01-04 22:32:04 +0000

    seq_len and visualize_prior

--- src/setup.sh
+++ src/setup.sh
@@ -8,6 +8,6 @@ pip install --upgrade pip
 python3.11 -m venv fmri
 source fmri/bin/activate
 
-pip install numpy matplotlib tqdm scikit-image jupyterlab accelerate webdataset clip pandas matplotlib ftfy regex kornia umap-learn h5py torchvision torch==2.0.1 diffusers deepspeed
+pip install numpy matplotlib tqdm scikit-image jupyterlab accelerate webdataset clip pandas matplotlib ftfy regex kornia umap-learn h5py torchvision torch==2.0.1 diffusers deepspeed omegaconf pytorch-lightning==2.0.1
 
 pip install git+https://github.com/openai/CLIP.git --no-deps
