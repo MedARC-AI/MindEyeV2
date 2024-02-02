2397c487255793362ebbef8035560a0b18e8d7eb
Paul Scotti <paulpauzle@gmail.com>
2024-02-02 12:43:02 -0500

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -8,6 +8,6 @@ pip install --upgrade pip
 python3.11 -m venv fmri
 source fmri/bin/activate
 
-pip install numpy matplotlib tqdm scikit-image jupyterlab accelerate webdataset clip pandas matplotlib ftfy regex kornia umap-learn h5py torchvision torch==2.0.1 diffusers deepspeed omegaconf pytorch-lightning==2.0.1
-
+pip install numpy matplotlib jupyter jupyterlab_nvdashboard jupyterlab tqdm scikit-image accelerate webdataset pandas matplotlib einops ftfy regex kornia h5py open_clip_torch torchvision torch==2.2.0 transformers xformers torchmetrics diffusers==0.23.0 deepspeed wandb nilearn nibabel omegaconf pytorch-lightning==2.0.1 
 pip install git+https://github.com/openai/CLIP.git --no-deps
+pip install dalle2-pytorch
