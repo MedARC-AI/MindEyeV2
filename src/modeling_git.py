c64c11875372cc80d54d49e364d4ad5a3ef33021
Paul Scotti <paulpauzle@gmail.com>
2024-02-03 11:50:57 -0500

    Update modeling_git.py

--- src/modeling_git.py
+++ src/modeling_git.py
@@ -13,6 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# This code was copied from https://github.com/enomodnara/BrainCaptioning/blob/main/modeling_git.py
+
 """PyTorch GIT model."""
 
 
