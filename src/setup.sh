b572417ee7589ada69bc1f55d61bc1c452399eb4
Paul Scotti <paulpauzle@gmail.com>
2024-02-04 10:06:25 -0500

    changing deepspeed version

--- src/setup.sh
+++ src/setup.sh
@@ -8,6 +8,6 @@ pip install --upgrade pip
 python3.11 -m venv fmri
 source fmri/bin/activate
 
-pip install numpy matplotlib==3.8.2 jupyter jupyterlab_nvdashboard jupyterlab tqdm scikit-image==0.22.0 accelerate==0.26.1 webdataset==0.2.86 pandas==2.2.0 einops ftfy regex kornia==0.7.1 h5py==3.10.0 open_clip_torch torchvision==0.17.0 torch==2.2.0 transformers==4.37.2 xformers==0.0.24 torchmetrics==1.3.0.post0 diffusers==0.23.0 deepspeed==0.13.1 wandb nilearn nibabel==0.10.3 omegaconf==2.3.0 pytorch-lightning==2.0.1 
+pip install numpy matplotlib==3.8.2 jupyter jupyterlab_nvdashboard jupyterlab tqdm scikit-image==0.22.0 accelerate==0.26.1 webdataset==0.2.86 pandas==2.2.0 einops ftfy regex kornia==0.7.1 h5py==3.10.0 open_clip_torch torchvision==0.17.0 torch==2.2.0 transformers==4.37.2 xformers==0.0.24 torchmetrics==1.3.0.post0 diffusers==0.23.0 deepspeed==0.12.2 wandb nilearn nibabel==0.10.3 omegaconf==2.3.0 pytorch-lightning==2.0.1 
 pip install git+https://github.com/openai/CLIP.git --no-deps
 pip install dalle2-pytorch
